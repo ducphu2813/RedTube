591d002a30aba5fd08abac379516616d49ddf2d6
Bein24 <nductri24@gmail.com>
2024-05-06 17:47:28 +0700

    Them phan thong bao cho homePage

--- hoc-laravel/resources/views/main/homePageBase.blade.php
+++ hoc-laravel/resources/views/main/homePageBase.blade.php
@@ -53,7 +53,172 @@
                 <i class="fa-solid fa-magnifying-glass" style="color: #fff; font-size: 14px;"></i>
             </button>
         </div>
+        {{-- Thông báo  --}}
+        <div class="notification">
+            <i class="fa-solid fa-bell" style="color: #ffffff;"></i>
+            <div class="wrapper-notify-item-list">
+                <header class="header-notify">
+                    <h4>Thông báo</h4>
+                </header>
+                <div class="wrapper-header-notify-list">
+                    <div class="header-notify-list">
+                        {{-- Từng item video  --}}
+                        <div class="header-notify-item">
+                            <a href="">
+                                <img class="channel-avatar-notify" src="https://yt3.ggpht.com/ytc/AIdro_kt-sUf4kFDrZ4iaFcyK4EHwVz-jlvQBwjZSA6hQ9ogPEg=s88-c-k-c0x00ffffff-no-rj" alt="">
+                                <div class="header-notify-info">
+                                    <div class="wrapper-header-notify-name">
+                                        <span class="header-notify-name">SỐNG CÙNG 3 EM GÁI TRONG 1 NHÀ !!! Xinh vừa thôi, ai chịu được - LOVE IS ALL AROUND: THE ROOM</span>
+                                    </div>
+                                    <div class="wrapper-header-notify-description">
+                                        <i class="fa-solid fa-circle" style="color: #000000;"></i>
+                                        <span class="header-notify-description">1 ngày trước</span>
+                                    </div>        
+                                </div>
+                                <img class="thumbnail-notify" src="https://i.ytimg.com/vi/0FFxM8WqtWI/hqdefault.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="header-notify-item">
+                            <a href="">
+                                <img class="channel-avatar-notify" src="https://yt3.ggpht.com/ytc/AIdro_kt-sUf4kFDrZ4iaFcyK4EHwVz-jlvQBwjZSA6hQ9ogPEg=s88-c-k-c0x00ffffff-no-rj" alt="">
+                                <div class="header-notify-info">
+                                    <div class="wrapper-header-notify-name">
+                                        <span class="header-notify-name">SỐNG CÙNG 3 EM GÁI TRONG 1 NHÀ !!! Xinh vừa thôi, ai chịu được - LOVE IS ALL AROUND: THE ROOM</span>
+                                    </div>
+                                    <div class="wrapper-header-notify-description">
+                                        <i class="fa-solid fa-circle" style="color: #000000;"></i>
+                                        <span class="header-notify-description">1 ngày trước</span>
+                                    </div>        
+                                </div>
+                                <img class="thumbnail-notify" src="https://i.ytimg.com/vi/0FFxM8WqtWI/hqdefault.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="header-notify-item">
+                            <a href="">
+                                <img class="channel-avatar-notify" src="https://yt3.ggpht.com/ytc/AIdro_kt-sUf4kFDrZ4iaFcyK4EHwVz-jlvQBwjZSA6hQ9ogPEg=s88-c-k-c0x00ffffff-no-rj" alt="">
+                                <div class="header-notify-info">
+                                    <div class="wrapper-header-notify-name">
+                                        <span class="header-notify-name">SỐNG CÙNG 3 EM GÁI TRONG 1 NHÀ !!! Xinh vừa thôi, ai chịu được - LOVE IS ALL AROUND: THE ROOM</span>
+                                    </div>
+                                    <div class="wrapper-header-notify-description">
+                                        <i class="fa-solid fa-circle" style="color: #000000;"></i>
+                                        <span class="header-notify-description">1 ngày trước</span>
+                                    </div>        
+                                </div>
+                                <img class="thumbnail-notify" src="https://i.ytimg.com/vi/0FFxM8WqtWI/hqdefault.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="header-notify-item">
+                            <a href="">
+                                <img class="channel-avatar-notify" src="https://yt3.ggpht.com/ytc/AIdro_kt-sUf4kFDrZ4iaFcyK4EHwVz-jlvQBwjZSA6hQ9ogPEg=s88-c-k-c0x00ffffff-no-rj" alt="">
+                                <div class="header-notify-info">
+                                    <div class="wrapper-header-notify-name">
+                                        <span class="header-notify-name">SỐNG CÙNG 3 EM GÁI TRONG 1 NHÀ !!! Xinh vừa thôi, ai chịu được - LOVE IS ALL AROUND: THE ROOM</span>
+                                    </div>
+                                    <div class="wrapper-header-notify-description">
+                                        <i class="fa-solid fa-circle" style="color: #000000;"></i>
+                                        <span class="header-notify-description">1 ngày trước</span>
+                                    </div>        
+                                </div>
+                                <img class="thumbnail-notify" src="https://i.ytimg.com/vi/0FFxM8WqtWI/hqdefault.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="header-notify-item">
+                            <a href="">
+                                <img class="channel-avatar-notify" src="https://yt3.ggpht.com/ytc/AIdro_kt-sUf4kFDrZ4iaFcyK4EHwVz-jlvQBwjZSA6hQ9ogPEg=s88-c-k-c0x00ffffff-no-rj" alt="">
+                                <div class="header-notify-info">
+                                    <div class="wrapper-header-notify-name">
+                                        <span class="header-notify-name">SỐNG CÙNG 3 EM GÁI TRONG 1 NHÀ !!! Xinh vừa thôi, ai chịu được - LOVE IS ALL AROUND: THE ROOM</span>
+                                    </div>
+                                    <div class="wrapper-header-notify-description">
+                                        <i class="fa-solid fa-circle" style="color: #000000;"></i>
+                                        <span class="header-notify-description">1 ngày trước</span>
+                                    </div>        
+                                </div>
+                                <img class="thumbnail-notify" src="https://i.ytimg.com/vi/0FFxM8WqtWI/hqdefault.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="header-notify-item">
+                            <a href="">
+                                <img class="channel-avatar-notify" src="https://yt3.ggpht.com/ytc/AIdro_kt-sUf4kFDrZ4iaFcyK4EHwVz-jlvQBwjZSA6hQ9ogPEg=s88-c-k-c0x00ffffff-no-rj" alt="">
+                                <div class="header-notify-info">
+                                    <div class="wrapper-header-notify-name">
+                                        <span class="header-notify-name">SỐNG CÙNG 3 EM GÁI TRONG 1 NHÀ !!! Xinh vừa thôi, ai chịu được - LOVE IS ALL AROUND: THE ROOM</span>
+                                    </div>
+                                    <div class="wrapper-header-notify-description">
+                                        <i class="fa-solid fa-circle" style="color: #000000;"></i>
+                                        <span class="header-notify-description">1 ngày trước</span>
+                                    </div>        
+                                </div>
+                                <img class="thumbnail-notify" src="https://i.ytimg.com/vi/0FFxM8WqtWI/hqdefault.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="header-notify-item">
+                            <a href="">
+                                <img class="channel-avatar-notify" src="https://yt3.ggpht.com/ytc/AIdro_kt-sUf4kFDrZ4iaFcyK4EHwVz-jlvQBwjZSA6hQ9ogPEg=s88-c-k-c0x00ffffff-no-rj" alt="">
+                                <div class="header-notify-info">
+                                    <div class="wrapper-header-notify-name">
+                                        <span class="header-notify-name">SỐNG CÙNG 3 EM GÁI TRONG 1 NHÀ !!! Xinh vừa thôi, ai chịu được - LOVE IS ALL AROUND: THE ROOM</span>
+                                    </div>
+                                    <div class="wrapper-header-notify-description">
+                                        <i class="fa-solid fa-circle" style="color: #000000;"></i>
+                                        <span class="header-notify-description">1 ngày trước</span>
+                                    </div>        
+                                </div>
+                                <img class="thumbnail-notify" src="https://i.ytimg.com/vi/0FFxM8WqtWI/hqdefault.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="header-notify-item">
+                            <a href="">
+                                <img class="channel-avatar-notify" src="https://yt3.ggpht.com/ytc/AIdro_kt-sUf4kFDrZ4iaFcyK4EHwVz-jlvQBwjZSA6hQ9ogPEg=s88-c-k-c0x00ffffff-no-rj" alt="">
+                                <div class="header-notify-info">
+                                    <div class="wrapper-header-notify-name">
+                                        <span class="header-notify-name">SỐNG CÙNG 3 EM GÁI TRONG 1 NHÀ !!! Xinh vừa thôi, ai chịu được - LOVE IS ALL AROUND: THE ROOM</span>
+                                    </div>
+                                    <div class="wrapper-header-notify-description">
+                                        <i class="fa-solid fa-circle" style="color: #000000;"></i>
+                                        <span class="header-notify-description">1 ngày trước</span>
+                                    </div>        
+                                </div>
+                                <img class="thumbnail-notify" src="https://i.ytimg.com/vi/0FFxM8WqtWI/hqdefault.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="header-notify-item">
+                            <a href="">
+                                <img class="channel-avatar-notify" src="https://yt3.ggpht.com/ytc/AIdro_kt-sUf4kFDrZ4iaFcyK4EHwVz-jlvQBwjZSA6hQ9ogPEg=s88-c-k-c0x00ffffff-no-rj" alt="">
+                                <div class="header-notify-info">
+                                    <div class="wrapper-header-notify-name">
+                                        <span class="header-notify-name">SỐNG CÙNG 3 EM GÁI TRONG 1 NHÀ !!! Xinh vừa thôi, ai chịu được - LOVE IS ALL AROUND: THE ROOM</span>
+                                    </div>
+                                    <div class="wrapper-header-notify-description">
+                                        <i class="fa-solid fa-circle" style="color: #000000;"></i>
+                                        <span class="header-notify-description">1 ngày trước</span>
+                                    </div>        
+                                </div>
+                                <img class="thumbnail-notify" src="https://i.ytimg.com/vi/0FFxM8WqtWI/hqdefault.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="header-notify-item">
+                            <a href="">
+                                <img class="channel-avatar-notify" src="https://yt3.ggpht.com/ytc/AIdro_kt-sUf4kFDrZ4iaFcyK4EHwVz-jlvQBwjZSA6hQ9ogPEg=s88-c-k-c0x00ffffff-no-rj" alt="">
+                                <div class="header-notify-info">
+                                    <div class="wrapper-header-notify-name">
+                                        <span class="header-notify-name">SỐNG CÙNG 3 EM GÁI TRONG 1 NHÀ !!! Xinh vừa thôi, ai chịu được - LOVE IS ALL AROUND: THE ROOM</span>
+                                    </div>
+                                    <div class="wrapper-header-notify-description">
+                                        <i class="fa-solid fa-circle" style="color: #000000;"></i>
+                                        <span class="header-notify-description">1 ngày trước</span>
+                                    </div>        
+                                </div>
+                                <img class="thumbnail-notify" src="https://i.ytimg.com/vi/0FFxM8WqtWI/hqdefault.jpg" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+        {{-- Hết phần thông báo --}}
         <div class="acc-box">
             <img src="{{ asset('resources/img/user.png') }}" alt="">
         </div>
